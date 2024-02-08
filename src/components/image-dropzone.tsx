a07cf2ea55109297a50fb4cdd5689a362ea884c9
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-08 12:26:32 +0530

    Create image dropzone component

---
+++ src/components/image-dropzone.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import Dropzone from "./ui/dropzone";
+import { Cross1Icon } from "@radix-ui/react-icons";
+import { Button } from "./ui/button";
+import { cn, formatFileSize } from "@/lib/utils";
+import React, { useState } from "react";
+
+const imageMimeTypes = [
+  "image/webp",
+  "image/tiff",
+  "image/svg+xml",
+  "image/png",
+  "image/jpeg",
+  "image/vnd.microsoft.icon",
+  "image/gif",
+  "image/bmp",
+];
+const allowedTypes = [
+  {
+    name: "image",
+    types: imageMimeTypes,
+  },
+];
+
+export type image = File;
+
+interface ImageDropzoneProps
+  extends Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    "value" | "onChange"
+  > {
+  className?: string;
+  maxFiles?: number;
+  maxSize?: number;
+  handleImages: (acceptedFiles: image[]) => void;
+}
+
+const ImageDropzone = React.forwardRef<HTMLDivElement, ImageDropzoneProps>(
+  (
+    {
+      className,
+      maxFiles = 5,
+      maxSize = 1024 * 1024 * 1,
+      handleImages,
+      ...props
+    },
+    ref,
+  ) => {
+    const [images, setFiles] = useState<image[]>([]);
+    const [dropzoneDisabled, setDropzoneDisabled] = useState<boolean>(false);
+
+    function updateImages(images: image[]) {
+      setFiles(images);
+      handleImages && handleImages(images);
+      if (images.length < maxFiles && dropzoneDisabled) {
+        setDropzoneDisabled(false);
+      } else if (!(images.length < maxFiles && !dropzoneDisabled)) {
+        setDropzoneDisabled(true);
+      }
+    }
+
+    function handleOnDrop(acceptedFiles: FileList | null) {
+      if (!(acceptedFiles && acceptedFiles.length > 0)) {
+        return;
+      }
+
+      const validFiles: image[] = [];
+      for (let i = 0; i < acceptedFiles.length; i++) {
+        if (!(images.length + validFiles.length < maxFiles)) {
+          break;
+        }
+        const file = acceptedFiles[i] as File;
+        const fileType = allowedTypes.find((allowedType) =>
+          allowedType.types.find((type) => type === file.type),
+        );
+
+        if (!fileType) {
+          continue;
+        }
+        if (file.size > maxSize) {
+          continue;
+        }
+
+        validFiles.push(file);
+
+        if (validFiles.length > 0) {
+          updateImages([...validFiles, ...images]);
+        }
+      }
+    }
+
+    return (
+      <div className="4xl:grid-cols-[repeat(4,1fr)] grid h-full w-full grid-cols-[repeat(2,1fr)] gap-2 2xl:grid-cols-[repeat(3,1fr)]">
+        <Dropzone
+          {...props}
+          classNameWrapper={cn("aspect-square", dropzoneDisabled && "hidden")}
+          disabled={dropzoneDisabled}
+          multiple
+          dropMessage="Drop images or click here"
+          handleOnDrop={handleOnDrop}
+          ref={ref}
+        />
+
+        {images.map((file, i) => {
+          return (
+            <div
+              key={i}
+              className="relative aspect-square h-full w-full rounded-md border border-input bg-transparent p-0 shadow-md"
+            >
+              <img
+                className="absolute left-0 top-0 h-full w-full rounded-md object-cover"
+                src={
+                  typeof file === "string" ? file : URL.createObjectURL(file)
+                }
+                alt={typeof file === "string" ? file : file.name}
+              />
+
+              <Button
+                variant="secondary"
+                size="icon"
+                type="button"
+                className="absolute right-0 top-0 -translate-x-1/4 translate-y-1/4 transform opacity-60"
+                onClick={() => {
+                  updateImages(images.filter((_, index) => index !== i));
+                }}
+              >
+                <Cross1Icon />
+              </Button>
+
+              {typeof file !== "string" && (
+                <div
+                  key={file.name}
+                  className="absolute bottom-0 w-full rounded-sm bg-gradient-to-b from-card/60 to-card/90 px-2 py-0.5"
+                >
+                  <p className="truncate text-sm font-medium">{file.name}</p>
+                  <span className="text-sm font-medium leading-3">
+                    {formatFileSize(file.size)}
+                  </span>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    );
+  },
+);
+
+export default ImageDropzone;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatFileSize(bytes?: number) {
+  if (!bytes) {
+    return "0 Bytes";
+  }
+  bytes = Number(bytes);
+  if (bytes === 0) {
+    return "0 Bytes";
+  }
+  const k = 1024;
+  const dm = 2;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
 }
