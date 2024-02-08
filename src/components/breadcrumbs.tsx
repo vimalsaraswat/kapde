34dae309e32b9cf2dc512ca124c1a7eb7e52e044
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-08 12:26:54 +0530

    Create Breadcrumbs component

---
+++ src/components/breadcrumbs.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+interface Breadcrumb {
+  label: string;
+  href: string;
+  active?: boolean;
+}
+
+export default function Breadcrumbs({
+  breadcrumbs,
+}: {
+  breadcrumbs: Breadcrumb[];
+}) {
+  return (
+    <nav aria-label="Breadcrumb" className="mb-6 block">
+      <ol className="flex text-xl md:text-2xl">
+        {breadcrumbs.map((breadcrumb, index) => (
+          <li
+            key={breadcrumb.href}
+            aria-current={breadcrumb.active}
+            className={`${breadcrumb.active ? "text-foreground" : "text-muted-foreground"}`}
+          >
+            <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+            {index < breadcrumbs.length - 1 ? (
+              <span className="mx-3 inline-block">/</span>
+            ) : null}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
