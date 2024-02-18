2555abd890d2318248bbe116c6353b9ffdb93011
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-18 13:15:27 +0530

    Create site config

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+const SiteConfig = {
+  name: "kapde",
+  description:
+    "An open source e-commerce marketplace for both buyers and sellers.",
+  url: "https://kapde.vercel.app",
+  links: {
+    twitter: "https://twitter.com/vimalsaraswat00",
+    github: "https://github.com/vimalsaraswat/kapde",
+  },
+};
+
+export default SiteConfig;
