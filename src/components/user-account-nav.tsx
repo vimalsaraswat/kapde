1579bf6d7e4bc94c7df62a1e75910ac734cb313c
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-18 13:11:38 +0530

    Cretate user account nav

---
+++ src/components/user-account-nav.tsx
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import { User } from "next-auth";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { AvatarProps } from "@radix-ui/react-avatar";
+import { getServerAuthSession } from "@/server/auth";
+import { Button } from "@/components/ui/button";
+
+export async function UserAccountNav() {
+  const user = (await getServerAuthSession())?.user;
+  if (!user)
+    return (
+      <Button variant="secondary" asChild>
+        <Link href="/api/auth/signin">Sign in</Link>
+      </Button>
+    );
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar user={user} className="h-8 w-8" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="w-[200px] truncate text-sm text-muted-foreground">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/dashboard">Dashboard</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/dashboard/settings">Settings</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="cursor-pointer">
+          <Link href="/api/auth/signout">Sign out</Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+interface UserAvatarProps extends AvatarProps {
+  user: Pick<User, "image" | "name">;
+}
+
+export function UserAvatar({ user, ...props }: UserAvatarProps) {
+  return (
+    <Avatar {...props}>
+      {user.image ? (
+        <AvatarImage alt="Picture" src={user.image} />
+      ) : (
+        <AvatarFallback>
+          <span className="sr-only">{user.name}</span>
+        </AvatarFallback>
+      )}
+    </Avatar>
+  );
+}
