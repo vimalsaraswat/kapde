51f3c78003a318a9d7a624235e0638b534c367fb
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-25 15:30:26 +0530

    Add search and infinite products scroll on buyer homepage

--- src/app/@buyer/page.tsx
+++ src/app/@buyer/page.tsx
@@ -1,3 +1,37 @@
-export default async function Page() {
-  return <main>Buyer</main>;
+import Products from "@/components/products";
+import Search from "@/components/search";
+import { ProductCategories } from "@prisma/client";
+import { notFound } from "next/navigation";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    category: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const category = (searchParams?.category || "").toUpperCase();
+  if (category && !(category in ProductCategories)) return notFound();
+
+  return (
+    <div className="mx-auto mt-4 sm:w-3/4">
+      <div className="flex w-full items-center justify-between">
+        <h2 className="text-2xl capitalize">
+          {category ? category.toLowerCase() : "All Products"}
+        </h2>
+      </div>
+
+      <Search
+        placeholder="Search products..."
+        className="mx-auto my-2 max-w-md md:my-4"
+      />
+
+      <Products
+        query={query}
+        category={(category as ProductCategories) ?? undefined}
+      />
+    </div>
+  );
 }
