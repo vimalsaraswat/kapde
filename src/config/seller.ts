9dcdccbced571747832d968b07b75df6b29459c8
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-25 17:06:11 +0530

    Create seller config

---
+++ src/config/seller.ts
@@ -0,0 +1,21 @@
+const SellerConfig = {
+  nav: [
+    {
+      title: "Products",
+      href: "/products",
+      icon: "post",
+    },
+    {
+      title: "Billing",
+      href: "/billing",
+      icon: "billing",
+    },
+    {
+      title: "Settings",
+      href: "/settings",
+      icon: "settings",
+    },
+  ],
+};
+
+export default SellerConfig;
